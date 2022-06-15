41a89d252a4c462fa323627e24c0ffe33077c172
Icaro Apolo <apoloraci@gmail.com>
2022-06-14 21:35:44 -0300

    feat(components): create header component and begin home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+
+import { Container, SearchContainer, Input, SearchButton } from './styles';
+import { Feather } from '@expo/vector-icons';
+
+import Header from '../../components/Header';
 
 function Home() {
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <Container>
+            <Header title="React Prime Video" />
+            <SearchContainer>
+                <Input
+                    placeholder="Ex Transformers"
+                    placeholderTextColor="#DDD"
+                />
+                <SearchButton>
+                    <Feather name="search" size={30} color="#FFF" />
+                </SearchButton>
+            </SearchContainer>
+        </Container>
     )
 }
 
