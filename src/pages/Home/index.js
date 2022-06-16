a675c217bbc9c6f9fa42211ffbcaeade5e1a3fcb
Icaro Apolo <apoloraci@gmail.com>
2022-06-15 21:55:29 -0300

    feat(sliders): create component sliderItem with flatList, and finish layout from HomePage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 
-import { Container, SearchContainer, Input, SearchButton } from './styles';
+import { ScrollView } from 'react-native';
+import {
+    Container,
+    SearchContainer,
+    Input,
+    SearchButton,
+    Title,
+    BannerButton,
+    Banner,
+    SliderMovie
+} from './styles';
 import { Feather } from '@expo/vector-icons';
 
 import Header from '../../components/Header';
+import SliderItem from '../../components/SliderItem';
 
 function Home() {
     return (
         <Container>
+
             <Header title="React Prime Video" />
             <SearchContainer>
                 <Input
@@ -18,6 +30,42 @@ function Home() {
                     <Feather name="search" size={30} color="#FFF" />
                 </SearchButton>
             </SearchContainer>
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <Title>Em cartaz</Title>
+                <BannerButton activeOpacity={0.8} onPress={() => alert('Teste')}>
+                    <Banner
+                        resizeMethod="resize"
+                        source={{ uri: 'https://images.unsplash.com/photo-1547658719-da2b51169166?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80' }}
+                    />
+                </BannerButton>
+
+                <SliderMovie
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                    data={[1, 2, 3, 4]}
+                    renderItem={({ item }) => <SliderItem />}
+                />
+
+                <Title>Populares</Title>
+
+                <SliderMovie
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                    data={[1, 2, 3, 4]}
+                    renderItem={({ item }) => <SliderItem />}
+                />
+
+                <Title>Mais Votados</Title>
+
+                <SliderMovie
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                    data={[1, 2, 3, 4]}
+                    renderItem={({ item }) => <SliderItem />}
+                />
+            </ScrollView>
+
         </Container>
     )
 }
