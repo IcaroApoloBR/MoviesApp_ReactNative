26c7dd2a900a426891f7bb7c755fdf201221450d
Icaro Apolo <apoloraci@gmail.com>
2022-06-19 16:51:18 -0300

    feat(list movies): use AsyncStorage, persist data and crud saved movies

---
+++ src/components/FavoriteItem/styles.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    padding: 14px;
+`;
+
+export const Title = styled.Text`
+    color: #FFF;
+    font-size: ${props => props.size}px;
+`;
+
+export const RateContainer = styled.View`
+    flex-direction: row;
+    align-items: center;
+    padding-left: 4px;
+`;
+
+export const Rate = styled.Text`
+    color: #FFF;
+    font-size: 12px;
+    padding-left: 4px;
+`;
+
+export const ActionContainer = styled.View`
+    flex-direction: row;
+    align-items: center;
+`;
+
+export const DetailButton = styled.TouchableOpacity`
+    width: 85%;
+    height: 30px;
+    background-color: #e72f49;
+    justify-content: center;
+    align-items: center;
+    border-radius: 30px;
+`;
+
+export const DeleteButton = styled.TouchableOpacity`
+    width: 15%;
+    height: 30px;
+    justify-content: center;
+    align-items: center;
+`;
--- src/pages/Movies/index.js
+++ src/pages/Movies/index.js
@@ -1,11 +1,66 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import Header from '../../components/Header';
+
+import { Container, ListMovies } from './styles';
+
+import { getMoviesSave, deleteMovie } from '../../utils/storage';
+
+import { useNavigation, useIsFocused } from '@react-navigation/native';
+import FavoriteItem from '../../components/FavoriteItem';
 
 function Movies() {
+    const navigation = useNavigation();
+    const isFocused = useIsFocused();
+
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        let isActive = true;
+
+        async function getFavoriteMovies() {
+            const result = await getMoviesSave('@reactflix');
+
+            if (isActive) {
+                setMovies(result);
+            }
+        }
+
+        if (isActive) {
+            getFavoriteMovies();
+        }
+
+        return () => {
+            isActive = false;
+        }
+
+    }, [isFocused]);
+
+    async function handleDelete(id) {
+        const result = await deleteMovie(id);
+        setMovies(result);
+    }
+
+    function navigateDetailsPage(item) {
+        navigation.navigate('Detail', { id: item.id })
+    }
+
     return (
-        <View>
-            <Text>Movies</Text>
-        </View>
+        <Container>
+            <Header title="Meus Filmes" />
+
+            <ListMovies
+                showsVerticalScrollIndicator={false}
+                data={movies}
+                keyExtractor={(item) => String(item.id)}
+                renderItem={({ item }) => (
+                    <FavoriteItem
+                        data={item}
+                        deleteMovie={handleDelete}
+                        navigatePage={() => navigateDetailsPage(item)}
+                    />
+                )}
+            />
+        </Container>
     )
 }
 
